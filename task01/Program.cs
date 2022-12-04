0b591c49f53cb624d12d5dd25fd6bc1a35f5344e
stanislav <79920025475@yandex.ru>
2022-12-04 19:21:12 +0500

    task01

---
+++ task01/Program.cs
@@ -0,0 +1,12 @@
+﻿// базовый код для массива
+Console.Write ("Введите количество элементов массива: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int [] array = new int [n];
+for (int i = 0; i < array.Length; i++ )
+{
+    Console.Write (" Введите элемент массива: ");
+    array [i] = Convert.ToInt32 (Console.ReadLine());
+}
+Console.WriteLine($"{string.Join(" , ", array)}");
+
+
