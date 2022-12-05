9b715b0842c63236d8090f6c18b8dc76ad603f3a
stanislav <79920025475@yandex.ru>
2022-12-05 08:17:01 +0500

    task02_function

---
+++ task02_function/Program.cs
@@ -0,0 +1,16 @@
+﻿// function
+//Найти максимум из 2-х чисел
+
+int numbersMax(int n, int m)
+{
+    if (n > m)
+        return n;
+    return m;
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numbersMax(n, m));
