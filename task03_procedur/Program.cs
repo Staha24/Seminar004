232d1e27a4d23396ae9117fd3e6bdfd92dc32b37
stanislav <79920025475@yandex.ru>
2022-12-05 09:04:00 +0500

    task03_procedur

---
+++ task03_procedur/Program.cs
@@ -0,0 +1,12 @@
+﻿// Процедуры
+
+void numbersMax (ref int a)
+{
+    a = a + 10;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+numbersMax(ref n);
+Console.WriteLine (n);
