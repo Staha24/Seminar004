0ba3982b3524127d0e72a71d052200cf319346c9
stanislav <79920025475@yandex.ru>
2022-12-06 10:15:30 +0500

    homeworke29

---
+++ homeworke29/Program.cs
@@ -0,0 +1,11 @@
+﻿// task29
+
+Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [ n ];
+for (int i = 0; i < array.Length; i++)
+{
+        Console.Write ("Введите элемент массива: ");
+        array [i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{ string.Join(",",array)}]");
