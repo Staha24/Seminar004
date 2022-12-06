dbe5567a32dcc442852ae487f41d9ac1099eb099
stanislav <79920025475@yandex.ru>
2022-12-06 08:43:51 +0500

    homeworke25

---
+++ homeworke25/Program.cs
@@ -0,0 +1,13 @@
+﻿// task25
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+    step = step * a;
+}
+Console.WriteLine("Ответ: " + step);
